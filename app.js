bfb67534e68237978b84e458218b6ad7bb993344
sk2597 <smruti.karade25@gmail.com>
2024-01-25 07:47:51 +0530

    Mongoose always behaves as if useNewUrlParser, useUnifiedTopology, and useCreateIndex are true, and useFindAndModify is false.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 //load config
-dotenv.config({path: './config/confit.env'});
+dotenv.config({path: './config/config.env'});
+
+connectDB();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
