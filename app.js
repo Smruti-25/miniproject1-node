454ea06e66e77b7f8042ef8fe43a8766b7101c85
sk2597 <smruti.karade25@gmail.com>
2024-01-28 23:18:18 +0530

    update story route

--- app.js
+++ app.js
@@ -21,8 +21,20 @@ connectDB();
 const app = express();
 
 // Body parser
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Method override
+app.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+      // look in urlencoded POST bodies and delete it
+      let method = req.body._method
+      delete req.body._method
+      return method
+    }
+  })
+);
 
 //logging
 if(process.env.NODE_ENV === 'development'){
